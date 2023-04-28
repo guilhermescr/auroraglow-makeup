50052a5debefa19e0e93f7a9b3bd647c27ca6cab
guilhermescr <guilhermechaves2005@hotmail.com>
2023-04-27 22:01:28 -0300

    feat: add open-search-bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,21 @@
 import AuroraGlowLogo from '../assets/imgs/auroraglow-makeup-logo.png';
 import SearchIcon from '../assets/icons/search-icon.svg';
+import { useState, useRef, useEffect } from 'react';
 
 export default function Header() {
+  const [isSearching, setIsSearching] = useState(false);
+  const searchInput = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isSearching) {
+      if (searchInput.current !== null) {
+        searchInput.current.focus();
+      }
+    }
+  }, [isSearching]);
+
   return (
-    <header className="header">
+    <header className={`header ${isSearching && 'white-bg'}`}>
       <img
         className="auroraglow-logo"
         src={AuroraGlowLogo}
@@ -22,7 +34,7 @@ export default function Header() {
           </ul>
         </nav>
 
-        <div className="search-bar">
+        <div className="search-bar" onClick={() => setIsSearching(true)}>
           <button type="button" className="search-bar__search-button">
             Search
           </button>
@@ -30,6 +42,20 @@ export default function Header() {
           <img src={SearchIcon} alt="Search Icon" />
         </div>
       </div>
+
+      {isSearching && (
+        <section className="open-search-bar">
+          <input
+            type="search"
+            name="search-input"
+            id="search-input"
+            ref={searchInput}
+            placeholder="Look For a Makeup..."
+          />
+
+          <img src={SearchIcon} alt="Search Icon" />
+        </section>
+      )}
     </header>
   );
 }
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,21 +1,12 @@
-import MakeupUnboxingVideo from '../../assets/videos/makeup-unboxing-background.mp4';
-import SearchIcon from '../../assets/icons/search-icon.svg';
+
+import MakeupUnboxingGIF from '../../assets/videos/auroraglow-makeup-unboxing-gif.gif';
 
 export default function Home() {
   return (
     <>
-      <div className="body-video-background">
-        <video autoPlay loop>
-          <source src={MakeupUnboxingVideo} type="video/mp4" />
-          <p>Your browser does not support the HTML5 video element</p>
-        </video>
+      <div className="makeup-unboxing-background">
+        <img src={MakeupUnboxingGIF} alt="Makeup Unboxing GIF" />
       </div>
-
-      <section className="open-search-bar">
-        <input type="search" name="search-input" id="search-input" />
-
-        <img src={SearchIcon} alt="Search Icon" />
-      </section>
 
       <h1>Home</h1>
     </>
